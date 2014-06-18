392b1d518d10e9a6644a67ee5f78b7f8af23527e
adou600 <adrien.nicolet@gmail.com>
2014-06-18 09:05:15 +0200

    new MVC5 project

---
+++ MVC5TDDExperiments/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC5TDDExperiments.MvcApplication" Language="C#" %>
---
+++ MVC5TDDExperiments/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC5TDDExperiments.Startup))]
+namespace MVC5TDDExperiments
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

5beda588ba7042ced44a266bc08b6359a64a6aa5
adou600 <adrien.nicolet@gmail.com>
2014-06-18 09:32:34 +0200

    Create model for the application

---
+++ MVC5TDDExperiments/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC5TDDExperiments.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public string Author { get; set; }
+    }
+}
